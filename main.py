fb5bf24f4e87d7cd2eefa72f028df74913925c8a
yakovtfi <205momi@gmail.com>
2025-10-23 12:56:20 +0300

    Refactor import statements in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from utils.deck import init_game, play_round
 if __name__ == "__main__":
-    from utils import init_game, play_round
     game_state = init_game( )
     players = game_state['players']
     play_round(players[0], players[1])
+
+
